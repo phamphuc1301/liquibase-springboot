3f8f27ffcf9026b1888b5c8d129fe8a9b21bf691
namnh24 <namnh24@fsoft.com.vn>
2018-12-12 23:35:36 +0700

    Initial commit

---
+++ src/main/java/com/example/liquibasespringboot/LiquibaseSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.liquibasespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseSpringbootApplication.class, args);
+    }
+
+}
+
