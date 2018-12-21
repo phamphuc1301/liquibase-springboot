634a389b1edf0a3942217299ee9b7a07c15adf71
namnh24 <namnh24@fsoft.com.vn>
2018-12-22 00:40:25 +0700

    Connect database to heroku

--- src/main/java/com/example/liquibasespringboot/controller/HelloController.java
+++ src/main/java/com/example/liquibasespringboot/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.liquibasespringboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api")
+@RestController
+@RequestMapping("/api")
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
